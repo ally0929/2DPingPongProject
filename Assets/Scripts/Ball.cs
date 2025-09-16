8d62772a5242f93f694b555d75594f2fd2814875
Allyson <ahussey@rollins.edu>
2025-09-16 13:34:11 -0400

    Ball.cs Movement
    Move speed

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,17 +1,18 @@
-using UnityEngine;
+ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
    
     public Rigidbody2D rb2d;
     public float maxInitialAngle = 0.67f;
+    public float moveSpeed = 1f;
     
     private void Start()
     {
         Vector2 dir = Vector2.left;
         dir.y = Random.Range(-maxInitialAngle, maxInitialAngle);
 
-        rb2d.linearVelocity = Vector2.left;
+        rb2d.velocity = dir * moveSpeed;
 
     }
 
