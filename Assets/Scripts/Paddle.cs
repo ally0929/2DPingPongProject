2d0295c14104f7e2b8f124b14b85bdcbedab8228
Allyson <ahussey@rollins.edu>
2025-09-16 18:04:36 -0400

    Paddle.cs

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -6,3 +6,36 @@ public class Paddle : MonoBehaviour
   public float id;
   public float moveSpeed = 2f;
 }
+
+private void Update()
+ 
+{
+  float movement = ProcessInput();
+  Move(movement);
+}
+
+private float ProcessInput()
+
+{
+  float movement = 0f;
+  switch(id)
+  {
+    case 1:
+      movement = Input.GetAxis("MovePlayer1");
+      break;
+
+    case 2:
+      movement = Input.GetAxis("MovePlayer2");
+      break;
+
+  }
+  return movement;
+}
+
+private void Move(float movement)
+{
+  Vector2 velo = rb2d.velocity;
+  velo.y = moveSpeed * movement;
+  rb2d.velocity = velo; 
+
+}
