8bcd9933c9610b7a3d96c561c17d3778bd9c574a
Allyson <ahussey@rollins.edu>
2025-09-16 13:29:23 -0400

    Updated Ball.cs

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,6 +3,17 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
    
-   
+    public Rigidbody2D rb2d;
+    public float maxInitialAngle = 0.67f;
+    
+    private void Start()
+    {
+        Vector2 dir = Vector2.left;
+        dir.y = Random.Range(-maxInitialAngle, maxInitialAngle);
+
+        rb2d.linearVelocity = Vector2.left;
+
+    }
+
 }
  
