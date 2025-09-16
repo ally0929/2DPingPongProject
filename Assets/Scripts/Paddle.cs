a61be322cb630118e7f5c9bcf37ba0bcf8ee2303
Allyson <ahussey@rollins.edu>
2025-09-16 18:07:00 -0400

    paddle.cs

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -5,37 +5,39 @@ public class Paddle : MonoBehaviour
   public Rigidbody2D rb2d;
   public float id;
   public float moveSpeed = 2f;
-}
 
-private void Update()
+
+  private void Update()
  
-{
-  float movement = ProcessInput();
-  Move(movement);
-}
-
-private float ProcessInput()
-
-{
-  float movement = 0f;
-  switch(id)
   {
-    case 1:
-      movement = Input.GetAxis("MovePlayer1");
-      break;
+    float movement = ProcessInput();
+    Move(movement);
+  }
 
-    case 2:
-      movement = Input.GetAxis("MovePlayer2");
-      break;
+  private float ProcessInput()
+
+  {
+    float movement = 0f;
+    switch(id)
+    {
+      case 1:
+        movement = Input.GetAxis("MovePlayer1");
+        break;
+
+      case 2:
+        movement = Input.GetAxis("MovePlayer2");
+        break;
+
+    }
+    return movement;
+  }
+
+  private void Move(float movement)
+  {
+    Vector2 velo = rb2d.velocity;
+    velo.y = moveSpeed * movement;
+    rb2d.velocity = velo; 
 
   }
-  return movement;
-}
-
-private void Move(float movement)
-{
-  Vector2 velo = rb2d.velocity;
-  velo.y = moveSpeed * movement;
-  rb2d.velocity = velo; 
 
 }
