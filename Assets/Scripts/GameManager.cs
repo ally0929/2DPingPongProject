0084ed10c6ff8739b9aa20125999afaf3e70ff69
Allyson <ahussey@rollins.edu>
2025-09-16 16:54:07 -0400

    Game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public int scorePlayer1, score Player2; 
+    public int scorePlayer1, scorePlayer2; 
 
     public void OnScoreZoneReached(int id){
         
