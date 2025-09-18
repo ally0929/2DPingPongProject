1322279b35838e8a640610ac14a860b5faf90c37
Allyson <ahussey@rollins.edu>
2025-09-17 20:27:49 -0400

    Game UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+
+    public GameUI GameUI;
     public int scorePlayer1, scorePlayer2; 
     public ScoreText scoreTextLeft, scoreTextRight;
     public System.Action onReset;
